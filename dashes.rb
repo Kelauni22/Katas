1c5c5bfa631242a18a456b624e273a682387fd0e
Kelauni22 <Kelauni.Cook@gmail.com>
2017-09-08 12:28:22 -0400

    added first kata

---
+++ dashes.rb
@@ -0,0 +1,39 @@
+=begin
+When you input a number of any size, any odd numbers will have a dash before and
+after the number. There are no dashes at the beginning or end of the returned
+string.
+=end
+
+def dasherize_number(num)
+  num_array =[]
+  #make num into and individual array of numbers
+  new_num = num.to_s.split('').map {|num| num.to_i}
+  if new_num[0] % 2 == 1  #check the first element
+    num_array += [new_num[0],"-"] #if odd, push number/dash into the array
+  elsif
+    num_array << new_num[0] #if even, push as is into array
+  end
+  #circle through each number
+  i = 1 #starting at the next element
+  while i < new_num.size
+    if new_num[i] % 2 == 0 #if the number is even, push into new array
+      num_array << new_num[i]
+    elsif
+      #if the number is odd, push two dashes and number into array
+      num_array += ["-", new_num[i],"-"]
+    end
+    i+=1
+  end
+  #take out the trailing dash
+  if num_array[-1] == "-" then num_array.pop end
+  #take out double dashes
+  i=0
+  while i < num_array.size
+    if (num_array[i] == "-" && num_array[i-1] == "-")
+      num_array.delete_at(i)
+    end
+    i+=1;
+  end
+  #join everything in the array to return a final string
+  final_string = num_array.join("")
+end
