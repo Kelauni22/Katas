7200a379f38b6f84a03cdd8cd32195afa79f82ad
Kelauni22 <Kelauni.Cook@gmail.com>
2017-09-13 13:41:40 -0400

    replaces elsif w/ else and added kata answer to bottom of dasherize.rb

--- dashes.rb
+++ dashes.rb
@@ -10,7 +10,7 @@ def dasherize_number(num)
   new_num = num.to_s.split('').map {|num| num.to_i}
   if new_num[0] % 2 == 1  #check the first element
     num_array += [new_num[0],"-"] #if odd, push number/dash into the array
-  elsif
+  else
     num_array << new_num[0] #if even, push as is into array
   end
   #circle through each number
@@ -18,7 +18,7 @@ def dasherize_number(num)
   while i < new_num.size
     if new_num[i] % 2 == 0 #if the number is even, push into new array
       num_array << new_num[i]
-    elsif
+    else
       #if the number is odd, push two dashes and number into array
       num_array += ["-", new_num[i],"-"]
     end
@@ -36,4 +36,29 @@ def dasherize_number(num)
   end
   #join everything in the array to return a final string
   final_string = num_array.join("")
+end
+
+#Below is the kata answer (a more simple way to write this)
+
+def dasherize_number(num)
+  num_s = num.to_s
+
+  result = ""
+
+  idx = 0
+  while idx < num_s.length
+    digit = num_s[idx].to_i
+
+    if (idx > 0)
+      prev_digit = num_s[idx - 1].to_i
+      if (prev_digit % 2 == 1) || (digit % 2 == 1)
+        result += "-"
+      end
+    end
+    result += num_s[idx]
+
+    idx += 1
+  end
+
+  return result
 end
